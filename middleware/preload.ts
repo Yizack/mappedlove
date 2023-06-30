4d67dd29f35a8f94118720c54a42bfef55e17217
Yizack Rangel <yizackr@gmail.com>
2023-06-30 06:52:00 +0200

    chore: preload banners

---
+++ middleware/preload.ts
@@ -0,0 +1,15 @@
+export default defineNuxtRouteMiddleware(() => {
+  if (process.server) {
+    return;
+  }
+  
+  const banners = [
+    "fields.jpg",
+    "heart.jpg"
+  ];
+
+  banners.forEach((banner) => {
+    const img = new Image();
+    img.src = `/images/illustrations/${banner}`;
+  });
+});
