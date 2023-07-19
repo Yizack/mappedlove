ebc9e1d2b0c342004d155c0cec9fe4ab01eb6796
Yizack Rangel <yizackr@gmail.com>
2023-07-19 04:25:01 -0500

    chore: error page

---
+++ error.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { statusCode, message } = useError().value;
+</script>
+
+<template>
+  <div class="container vh-100 d-flex justify-content-center align-items-center">
+    <div>
+      <h1>Error <span class="text-primary">{{ statusCode }}</span></h1>
+      <h5>Sorry, an error has occured. {{ message }}</h5>
+      <p>Go back: <NuxtLink to="/" class="text-decoration-underline">mappedlove.com</NuxtLink></p>
+    </div>
+  </div>
+</template>
