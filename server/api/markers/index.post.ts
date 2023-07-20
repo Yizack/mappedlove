4815d1a1f906f912b2528f2593cf7da76910a083
Yizack Rangel <yizackr@gmail.com>
2023-07-19 22:33:27 -0500

    fix: d1 await

--- server/api/markers/index.post.ts
+++ server/api/markers/index.post.ts
@@ -5,7 +5,7 @@ export default eventHandler(async (event) : Promise<MappedLoveMarker> => {
   const body = await readBody(event);
   const DB = useDb();
 
-  const last = DB.select().from(tables.markers).where(eq(tables.markers.bond, user.bond.id)).orderBy(desc(tables.markers.order)).limit(1).get();
+  const last = await DB.select().from(tables.markers).where(eq(tables.markers.bond, user.bond.id)).orderBy(desc(tables.markers.order)).limit(1).get();
   return DB.insert(tables.markers).values({
     lat: body.lat,
     lng: body.lng,
