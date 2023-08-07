539ea2fab60143cbf0cc0528c963d158ee3998f7
Yizack Rangel <yizackr@gmail.com>
2023-08-07 06:33:25 -0500

    chore: statusmessage || message

--- error.vue
+++ error.vue
@@ -1,12 +1,12 @@
 <script setup>
-const { statusCode, message } = useError().value;
+const { statusCode, statusMessage, message } = useError().value;
 </script>
 
 <template>
   <div class="container vh-100 d-flex justify-content-center align-items-center">
     <div>
       <h1>Error <span class="text-primary">{{ statusCode }}</span></h1>
-      <h5>Sorry, an error has occured. {{ message }}</h5>
+      <h5>Sorry, an error has occured. {{ statusMessage || message }}</h5>
       <p>Go back: <NuxtLink to="/" class="text-decoration-underline">mappedlove.com</NuxtLink></p>
     </div>
   </div>
