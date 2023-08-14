02bb7e44f444a8b6c0db67d1f4502ee03810cd6c
Yizack Rangel <yizackr@gmail.com>
2023-08-13 21:47:56 -0500

    revert: marker images

--- components/MapView.vue
+++ components/MapView.vue
@@ -73,8 +73,8 @@ export default {
             <div class="carousel-inner">`
               + stories.map(({ id }, index) => {
                 return `
-                <div class="carousel-item ${!index ? "active" : ""} rounded-circle overflow-hidden position-relative" style="height: 100px; width: 100px">
-                  <img src="${getStoryImageFromUser(id)}" class="w-100 position-absolute start-50 top-50 translate-middle">
+                <div class="carousel-item ${!index ? "active" : ""}" style="height: 100px; width: 100px">
+                  <img src="${getStoryImageFromUser(id)}" class="rounded-circle" width="100" height="100" style="object-fit: contain">
                 </div>`;
               }).join("")
             + `</div>
