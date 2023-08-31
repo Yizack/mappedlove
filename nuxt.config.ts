a9647d0f91225c99802f835a7488ce8fa6a8048c
Yizack Rangel <yizackr@gmail.com>
2023-08-31 13:32:19 -0500

    chore: max scale

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
+      viewport: "width=device-width, initial-scale=1, maximum-scale=1",
       title: SITE.name,
       htmlAttrs: {
         lang: "en"
