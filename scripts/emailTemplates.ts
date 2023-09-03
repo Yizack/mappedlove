4aaa7249e9514293f80fee97b58e917c58b4bd58
Yizack Rangel <yizackr@gmail.com>
2023-09-02 21:32:57 -0500

    chore: templates export no quotes on properties

--- scripts/emailTemplates.ts
+++ scripts/emailTemplates.ts
@@ -15,7 +15,17 @@ try {
 catch {
   content = "";
 }
-const write_content = `export const templates = ${JSON.stringify(templates)};`;
+
+
+const keys = Object.keys(templates);
+const values = Object.values(templates);
+let result = "";
+for (let i = 0; i < keys.length; i++) {
+  result += ` ${keys[i]}: ${JSON.stringify(values[i])}` + (i < keys.length - 1 ? "," : " ");
+}
+
+const write_content = `export const templates = {${result}};`;
+
 
 if (content !== write_content) writeFileSync("./server/utils/mustache.ts", write_content, "utf8");
 console.info("Email templates loaded to server.");
