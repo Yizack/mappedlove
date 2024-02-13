d1dd6a3760cd3131879a19781c87732c852f08fe
Yizack Rangel <yizackr@gmail.com>
2024-02-13 16:28:24 -0500

    fix: enter settings without bond

--- middleware/session.ts
+++ middleware/session.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn, user } = useUserSession();
   if (!loggedIn.value) return navigateTo("/", { replace: true });
-  else if (to.name !== "app") {
+  else if (to.name !== "app" && to.name !== "app-settings") {
     if (!user.value || !user.value.bond || !user.value.bond.bonded) {
       return navigateTo("/app", { replace: true });
     }
