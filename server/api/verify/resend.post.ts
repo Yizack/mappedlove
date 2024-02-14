8c5c1d96fc0d999b1f8f8cf6c0c94166972f31c5
Yizack Rangel <yizackr@gmail.com>
2024-02-14 00:13:46 -0500

    fix: resend verify link

--- server/api/verify/resend.post.ts
+++ server/api/verify/resend.post.ts
@@ -7,16 +7,22 @@ export default defineEventHandler(async (event) : Promise<{ email: string }> => 
   const config = useRuntimeConfig(event);
   const DB = useDb();
   const user = await DB.select({
-    name: tables.users.name
+    id: tables.users.id,
+    name: tables.users.name,
+    email: tables.users.email,
+    updatedAt: tables.users.updatedAt
   }).from(tables.users).where(eq(tables.users.email, email)).get();
 
   if (!user) throw createError({ statusCode: ErrorCode.NOT_FOUND, message: "user_not_found" });
+
+  const fields = [user.id, user.email, user.updatedAt];
+  const code = hash(fields.join(""), config.secure.salt);
 
   const { name } = user;
   const url = process.dev ? "http://localhost:5173" : "https://mappedlove.com";
 
   const template_strings = {
-    verify_link: `${url}/verify/${encodeURIComponent(btoa(email))}/${token}`
+    verify_link: `${url}/verify/${encodeURIComponent(btoa(email))}/${code}`
   };
 
   const html = Mustache.render(templates.verifyAccount, template_strings);
