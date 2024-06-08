b6b2db8fd28967e31eae65e909519e8be54080f3
Yizack Rangel <yizackr@gmail.com>
2024-06-08 05:55:18 -0500

    chore: contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,5 +3,26 @@ definePageMeta({ layout: "utils" });
 </script>
 
 <template>
-  <UnderConstruction />
+  <main>
+    <div class="col-md-11 col-lg-8 m-auto px-3 py-4 px-lg-4 bg-body rounded-3 shadow">
+      <h2>Our emails</h2>
+      <p class="mb-4">Please use the appropriate email for your inquiry.</p>
+      <div class="row g-3">
+        <div class="col-lg-3 text-center">
+          <div class="bg-primary rounded-circle mx-auto d-flex align-items-center justify-content-center mb-2" :style="{ height: '6rem', width: '6rem' }">
+            <Icon name="solar:lightbulb-minimalistic-bold-duotone" class="text-secondary" size="4rem" />
+          </div>
+          <h4 class="m-0">Support</h4>
+          <a :href="`mailto:support@${SITE.domain}`">support@{{ SITE.domain }}</a>
+        </div>
+        <div class="col-lg-3 text-center">
+          <div class="bg-primary rounded-circle mx-auto d-flex align-items-center justify-content-center mb-2" :style="{ height: '6rem', width: '6rem' }">
+            <Icon name="solar:suitcase-bold-duotone" class="text-secondary" size="4rem" />
+          </div>
+          <h4 class="m-0">Legal</h4>
+          <a :href="`mailto:legal@${SITE.domain}`">legal@{{ SITE.domain }}</a>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
