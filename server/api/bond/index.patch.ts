e14bd0cd92cd9ac073420476b2b1854bd948e655
Yizack Rangel <yizackr@gmail.com>
2024-06-14 07:20:09 -0500

    fix: session bond on edit

--- server/api/bond/index.patch.ts
+++ server/api/bond/index.patch.ts
@@ -6,11 +6,7 @@ export default eventHandler(async (event) : Promise<MappedLoveBond> => {
   if (!user.bond) throw createError({ statusCode: ErrorCode.NOT_FOUND, message: "bond_not_found" });
 
   const body = await readValidatedBody(event, (body) => z.object({
-    code: z.string().optional(),
-    partner1: z.number().optional().nullable(),
-    partner2: z.number().optional().nullable(),
     coupleDate: z.number().optional().nullable(),
-    bonded: z.number().optional(),
     public: z.number().optional()
   }).safeParse(body));
 
@@ -20,15 +16,22 @@ export default eventHandler(async (event) : Promise<MappedLoveBond> => {
 
   const DB = useDb();
   const bond = await DB.update(tables.bonds).set({
-    id: user.bond.id,
-    code: form.code,
-    partner1: form.partner1,
-    partner2: form.partner2,
     coupleDate: form.coupleDate,
-    bonded: form.bonded,
     public: form.public,
     updatedAt: Date.now()
   }).where(and(eq(tables.bonds.code, user.bond.code), or(eq(tables.bonds.partner1, user.id), eq(tables.bonds.partner2, user.id)))).returning().get();
-  await setUserSession(event, { user: { ...user, bond } });
+
+  await setUserSession(event, {
+    user: {
+      ...user,
+      bond: {
+        ...user.bond,
+        coupleDate: bond.coupleDate,
+        public: bond.public,
+        updatedAt: bond.updatedAt
+      }
+    }
+  });
+
   return bond;
 });
