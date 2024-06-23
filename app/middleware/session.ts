0fea0f202d3b79bacc11599b4ccb4f6f71f2bde2
Yizack Rangel <yizackr@gmail.com>
2024-06-22 22:48:16 -0500

    chore: improve session middleware code

--- app/middleware/session.ts
+++ app/middleware/session.ts
@@ -1,13 +1,15 @@
 export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn, user } = useUserSession() as MappedLoveSessionComposable;
+
   if (!loggedIn.value) return navigateTo("/login", { replace: true });
-  else if (to.name !== "app" && to.name !== "app-settings") {
-    if (!user.value || !user.value.bond || !user.value.bond.bonded) {
-      if (import.meta.client && (!user.value.bond || !user.value.bond.bonded)) {
-        const { $toasts } = useNuxtApp();
-        $toasts.add({ message: t("need_bond"), success: false });
-      }
-      return navigateTo("/app", { replace: true });
+
+  const userBonded = user.value?.bond?.bonded;
+
+  if (to.name !== "app" && to.name !== "app-settings" && !userBonded) {
+    if (import.meta.client && !userBonded) {
+      const { $toasts } = useNuxtApp();
+      $toasts.add({ message: t("need_bond"), success: false });
     }
+    return navigateTo("/app", { replace: true });
   }
 });
