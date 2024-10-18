b0a0ee6e6d7708542522509b0e852fd0478a8c4a
Yizack Rangel <yizackr@gmail.com>
2024-10-18 09:00:53 -0500

    chore: image-upload class

--- app/components/navbar/NavbarApp.vue
+++ app/components/navbar/NavbarApp.vue
@@ -43,7 +43,7 @@ const logOut = () => {
       <div class="ms-3">
         <div v-if="loggedIn" class="nav-item dropdown">
           <button class="button btn btn-primary rounded-pill dropdown-toggle d-flex align-items-center justify-content-center" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <div v-if="user.showAvatar && user.hash" id="image-upload" class="text-center">
+            <div v-if="user.showAvatar && user.hash" class="image-upload text-center">
               <label class="rounded-circle bg-body-tertiary position-relative overflow-hidden d-flex" style="width: 24px; height: 24px;">
                 <img :src="`${getAvatarImage(user.hash)}?updated=${user.updatedAt}`" alt="avatar" width="24" height="24" class="img-fluid">
               </label>
