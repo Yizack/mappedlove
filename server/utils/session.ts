3cc981371ca8fffe8d405c5a98fca6a47e7b240b
Yizack Rangel <yizackr@gmail.com>
2025-02-06 01:34:08 -0500

    chore(session): no need to pass full session

--- server/utils/session.ts
+++ server/utils/session.ts
@@ -2,11 +2,21 @@ import { useSession, type H3Event, type SessionConfig } from "h3";
 import type { UserSession } from "#auth-utils";
 
 export const setUserSessionNullish = async (event: H3Event, data: UserSession, config?: Partial<SessionConfig>) => {
-  data.maxAge = config?.maxAge ?? data.maxAge;
-  const session = await useSession<UserSession>(event, {
+  const finalConfig = {
     ...useRuntimeConfig(event).session,
-    maxAge: data.maxAge
-  });
+    ...config
+  };
+
+  let session = await useSession<UserSession>(event, finalConfig);
+
+  if (session.data.maxAge && config?.maxAge === undefined) {
+    session = await useSession<UserSession>(event, {
+      ...finalConfig,
+      maxAge: session.data.maxAge as number
+    });
+  }
+
+  if (config?.maxAge) data.maxAge = config?.maxAge;
   await session.update(data);
   return session.data;
 };
