7d27d1d3e7155f5340880aecf614aaab33c471aa
Yizack Rangel <yizackr@gmail.com>
2025-02-08 04:43:53 -0500

    chore(datepicker): register component

--- app/plugins/vuedatepicker.client.ts
+++ app/plugins/vuedatepicker.client.ts
@@ -1,22 +1,11 @@
-import dp from "@vuepic/vue-datepicker";
-import type { App, Plugin } from "vue";
-
-const component = dp;
-type VueDatePickerComponent = typeof component;
-type VueDatePickerPlugin = VueDatePickerComponent & Plugin;
+import VueDatePicker from "@vuepic/vue-datepicker";
 
 declare module "vue" {
-  export interface GlobalComponents {
-    VueDatePicker: VueDatePickerComponent;
+  interface GlobalComponents {
+    VueDatePicker: typeof VueDatePicker;
   }
 }
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const VueDatePicker: VueDatePickerPlugin = ((): VueDatePickerPlugin => {
-    const installable = component as VueDatePickerPlugin;
-    installable.install = (app: App) => app.component("VueDatePicker", installable);
-    return installable;
-  })();
-
-  nuxtApp.vueApp.use(VueDatePicker);
+  nuxtApp.vueApp.component("VueDatePicker", VueDatePicker);
 });
