0f78af54f4f543bc5ce94ef903b31d50e89aa74e
Yizack Rangel <yizackr@gmail.com>
2025-02-27 22:02:14 -0500

    style: add dvh-100 class

--- app/layouts/access.vue
+++ app/layouts/access.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="layout" class="banner banner-access">
+  <div id="layout" class="banner banner-access dvh-100">
     <div class="container-fluid py-3 d-flex align-items-center">
       <slot />
     </div>
