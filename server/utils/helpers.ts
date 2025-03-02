2a39b6723a7b47b0b7bf20661bad009d8b4cd8bc
Yizack Rangel <yizackr@gmail.com>
2025-03-02 15:08:44 -0500

    refactor: prepare to hash encode to base64url

--- server/utils/helpers.ts
+++ server/utils/helpers.ts
@@ -7,7 +7,8 @@ const encoder = new TextEncoder();
 
 export const hash = async (string: string, salt?: string) => {
   const data = encoder.encode(salt ? string + salt : string);
-  return subtle.digest("SHA-256", data).then(hash => Buffer.from(hash).toString("hex"));
+  const base64 = await subtle.digest("SHA-256", data).then(hash => Buffer.from(hash).toString("base64"));
+  return base64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
 };
 
 export const createBondCode = (id: number) => {
