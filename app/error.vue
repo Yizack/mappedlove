3e1049dcd78eee67ad161808e2b7ad51a99abbf6
Yizack Rangel <yizackr@gmail.com>
2025-04-02 20:50:42 -0500

    chore: improve error messages

--- app/error.vue
+++ app/error.vue
@@ -10,7 +10,7 @@ defineProps<{
   <div class="container vh-100 d-flex justify-content-center align-items-center">
     <div class="bg-body p-5 rounded-3 shadow">
       <h1>{{ t("error") }} <span class="text-primary">{{ error.statusCode }}</span></h1>
-      <h5>{{ t("error_occurred") }}: {{ t(error.statusMessage || error.message || "error") }}</h5>
+      <h5>{{ t("error_occurred") }}: {{ t(error.message || error.statusMessage || "error") }}</h5>
       <p>{{ t("go_back") }}: <NuxtLink to="/" class="text-decoration-underline">{{ SITE.domain }}</NuxtLink></p>
     </div>
   </div>
