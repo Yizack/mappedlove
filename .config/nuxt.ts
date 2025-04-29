16b8429e55bdc03d13e131df674d13fd8b2b9b0d
Yizack Rangel <yizackr@gmail.com>
2025-04-29 10:37:56 -0500

    chore: add wrangler config

--- .config/nuxt.ts
+++ .config/nuxt.ts
@@ -4,7 +4,7 @@ import { SITE } from "../shared/utils/site";
 export default defineNuxtConfig({
   future: { compatibilityVersion: 4 },
   devtools: { enabled: true },
-  compatibilityDate: "2024-07-02",
+  compatibilityDate: "2025-04-29",
   app: {
     head: {
       charset: "utf-8",
