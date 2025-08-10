6537dceb12f0197e211db9cbf87a6034e54402e3
Yizack Rangel <yizackr@gmail.com>
2025-08-10 03:03:13 +0200

    test: add test suite (#183)

---
+++ .config/vitest.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vitest/config";
+import { defineVitestProject } from "@nuxt/test-utils/config";
+
+export default defineConfig({
+  test: {
+    projects: [
+      {
+        test: {
+          name: "unit",
+          include: ["test/{e2e,unit}/*.{test,spec}.ts"],
+          environment: "node",
+          setupFiles: ["test/e2e/__setup__.ts"]
+        }
+      },
+      await defineVitestProject({
+        test: {
+          name: "nuxt",
+          include: ["test/nuxt/*.{test,spec}.ts"],
+          environment: "nuxt"
+        }
+      })
+    ]
+  }
+});
---
+++ test/e2e/__setup__.ts
@@ -0,0 +1,32 @@
+import { $fetch, fetch, setup } from "@nuxt/test-utils/e2e";
+import { beforeAll } from "vitest";
+
+await setup({ host: "http://localhost:3000" });
+
+const getSessionCookie = async () => {
+  const loginResponse = await fetch("/api/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: "test2@test.test",
+      password: "test",
+      remember: true
+    })
+  });
+
+  const setCookie = loginResponse.headers.getSetCookie();
+  const sessionCookie = setCookie.find(cookie => cookie.startsWith("nuxt-session="));
+  if (!sessionCookie) throw new Error("Session cookie not found");
+  return sessionCookie;
+};
+
+declare global {
+  var cookie: string;
+}
+
+beforeAll(async () => {
+  await $fetch("/_nitro/tasks/seed");
+  global.cookie = await getSessionCookie();
+});
