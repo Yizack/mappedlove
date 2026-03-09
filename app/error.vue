10157512d07e0971e4ceecfd7ddbfbe6222fa6d3
Yizack Rangel <yizackr@gmail.com>
2026-03-09 10:02:48 -0500

    chore: update error props

--- app/error.vue
+++ app/error.vue
@@ -12,14 +12,14 @@ const getErrorMessage = (error: NuxtError) => {
   if (Array.isArray(errors)) {
     return errors.map(e => e.message).join(", ");
   }
-  return errors || error.statusMessage;
+  return errors || error.statusText;
 };
 </script>
 
 <template>
   <div class="container vh-100 d-flex justify-content-center align-items-center">
     <div class="bg-body p-5 rounded-3 shadow">
-      <h1>{{ t("error") }} <span class="text-primary">{{ error.statusCode }}</span></h1>
+      <h1>{{ t("error") }} <span class="text-primary">{{ error.status }}</span></h1>
       <h5>{{ t("error_occurred") }}: {{ t(getErrorMessage(error) || "error") }}</h5>
       <p>{{ t("go_back") }}: <NuxtLink to="/" class="text-decoration-underline">{{ SITE.domain }}</NuxtLink></p>
     </div>
