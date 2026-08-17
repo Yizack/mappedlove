d83f35a389a8541d37990c695594472458f7df32
Yizack Rangel <yizackr@gmail.com>
2026-08-17 14:51:01 +0200

    chore(types): remove obsolete aliases

--- shared/types/index.d.ts
+++ shared/types/index.d.ts
@@ -1,14 +1,9 @@
-import type { ErrorCode } from "#shared/utils/errors";
-import type { Quota } from "#shared/utils/quotas";
-
 declare global {
   interface User extends MappedLoveUser {
     hash?: string;
     passwordless?: boolean;
     bond?: Omit<MappedLoveBond, "partners"> | null;
   }
-  type ErrorCode = typeof ErrorCode;
-  type Quota = typeof Quota;
 }
 
 export {};
